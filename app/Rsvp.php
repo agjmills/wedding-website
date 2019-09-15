6a87d0c17683ee86c8914c368788d440119febf2
Alex Mills <alex@asdfx.us>
2019-09-15 17:13:55 +0100

    added uuid on create rsvp

--- app/Rsvp.php
+++ app/Rsvp.php
@@ -3,10 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Rsvp extends Model
 {
     protected $table = 'rsvp';
 
     protected $fillable = ['name', 'email', 'telephone', 'attending', 'message'];
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($rsvp) {
+            $rsvp->uuid = Str::uuid();
+        });
+    }
 }
