5151b7e3bf9f7005c508dd036a3a822b79716c33
Alex Mills <alex@asdfx.us>
2019-12-28 17:31:05 +0000

    updated link email

--- resources/views/emails/order-of-service/information.blade.php
+++ resources/views/emails/order-of-service/information.blade.php
@@ -8,27 +8,23 @@
         our wedding, in February - as it's only a few weeks away!
     </p>
     <p>
-        We've updated the website, which now contains; the order of service with approximate timings, a list of nearby
+        We've updated the website, which now contains: the order of service with approximate timings, a list of nearby
         accommodation that you may wish to book, as well as some guidance if you wish to gift us something.
     </p>
     <ul>
         <li>
-            For the order of service, click here: {{ route('order-of-service') }}
+            <a href="{{ route('order-of-service') }}" target="_blank">Order of Service</a>
         </li>
         <li>
-            For information about the venue, click here: {{ route('venue') }}
+            <a href="{{ route('venue') }}" target="_blank">Venue Information</a>
         </li>
         <li>
-            For information about the nearby accommodation, click here: {{ route('accommodations') }}
+            <a href="{{ route('accommodations') }}" target="_blank">Nearby Accommodation</a>
         </li>
         <li>
-            For information about gifts, click here: {{ route('gifts') }}
+            <a href="{{ route('gifts') }}" target="_blank">Gift Guidance</a>
         </li>
     </ul>
-    <p>If you have any questions, or have any specific requirements which
-        aren't met by the choices provided, please <a href="{{ route('contact') }}">get in touch</a> with one of us, and
-        we will help as best we can.
-    </p>
 
     <p>
         If you have any questions, please get in touch ({{ route('contact') }}) with one of us, and we will help as best we can.
--- resources/views/emails/order-of-service/information_plain.blade.php
+++ resources/views/emails/order-of-service/information_plain.blade.php
@@ -1,9 +1,9 @@
 Dear {{ $rsvp->name }},
 
-We hope you have had a wonderful Christmas! Veronika &amp; I thought we would share some more information to you about
+We hope you have had a wonderful Christmas! Veronika & I thought we would share some more information to you about
 our wedding, in February - as it's only a few weeks away!
 
-We've updated the website, which now contains; the order of service with approximate timings, a list of nearby
+We've updated the website, which now contains: the order of service with approximate timings, a list of nearby
 accommodation that you may wish to book, as well as some guidance if you wish to gift us something.
 
 For the order of service, click here: {{ route('order-of-service') }}
@@ -13,6 +13,6 @@ For information about gifts, click here: {{ route('gifts') }}
 
 If you have any questions, please get in touch ({{ route('contact') }}) with one of us, and we will help as best we can.
 
-We hope you have a Happy New Year, and we look forward to seeing you at Crown &amp; Sandy's on 1st February 2020!
+We hope you have a Happy New Year, and we look forward to seeing you at Crown & Sandy's on 1st February 2020!
 
 Veronika & Alex
