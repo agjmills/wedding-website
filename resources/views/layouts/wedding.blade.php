baec0bf166cd20ed14566982c29fdb58f808f2f3
Alex Mills <alex@asdfx.us>
2019-12-28 17:36:32 +0000

    added google analytics

--- resources/views/layouts/wedding.blade.php
+++ resources/views/layouts/wedding.blade.php
@@ -1,6 +1,15 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-102488718-11"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-102488718-11');
+    </script>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title', 'Veronika & Alex')</title>
